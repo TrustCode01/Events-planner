a023ccd1987d4fad5daf6e486859a52f478309b3
TrustCode01 <trustcharingani@gmail.com>
2024-08-04 07:40:01 -0400

    implementing redux to manage add on page

--- src/avSlice.js
+++ src/avSlice.js
@@ -3,16 +3,55 @@ import { createSlice } from "@reduxjs/toolkit";
 export const avSlice = createSlice({
   name: "av",
   initialState: [
+        {
+        img: "https://pixabay.com/photos/business-computer-conference-20031/",
+        name: "Projectors",
+        cost: 200,
+        quantity: 0,
+    },
+    {
+        img: "https://pixabay.com/photos/speakers-blacks-sound-musicals-3747617/",
+        name: "Speaker",
+        cost: 35,
+        quantity: 0,
+    },
+    {//done img
+        img: "https://pixabay.com/photos/podcast-microphone-music-audio-7876792/",
+        name: "Microphones",
+        cost: 45,
+        quantity: 0,
+    },
+    {
+        img: "https://pixabay.com/images/download/whiteboard-2903269_640.png",
+        name: "Whiteboards",
+        cost: 80,
+        quantity: 0,
+    },
+
+    {
+        img: "https://pixabay.com/photos/sign-signpost-right-left-direction-490816",
+        name: "Signage",
+        cost: 80,
+        quantity: 0,
+    },
+
     
   ],
 
 
   reducers: {
     incrementAvQuantity: (state, action) => {
+        const item = state[action.payload];
+        if (item) {
+            item.quantity++;
+        }   
       
     },
     decrementAvQuantity: (state, action) => {
-     
+        const item = state[action.payload];
+        if (item && item.quantity > 0) {
+            item.quantity--;
+        } 
     },
   },
 });
